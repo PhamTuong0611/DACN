655852f39a6428aef5b09447b0b59ee133fda283
PhamTuong0611 <tuongpham184@gmail.com>
2025-11-22 15:27:29 +0700

    Delete tls_demo_package directory

--- tls_demo_package/scanner/modules/ct_monitor.py
+++
@@ -1,8 +0,0 @@
-"""Theo dõi chứng chỉ trên Certificate Transparency (placeholder)."""
-
-from typing import List
-
-
-async def fetch_ct_events(hostname: str) -> List[str]:
-    """Placeholder: trả về danh sách rỗng cho tới khi tích hợp API CT."""
-    return []
